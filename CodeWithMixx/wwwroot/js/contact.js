a577173ef8c50c6b1623cca1a3006f16af73fcee
Milan Nikolić <milan.miki.nikolic2002@gmail.com>
2026-06-09 11:55:25 +0200

    fix(contact, pricing): Adjust data-type attributes

--- CodeWithMixx/wwwroot/js/contact.js
+++ CodeWithMixx/wwwroot/js/contact.js
@@ -16,6 +16,8 @@ export function initContact() {
         
         queryType.value = target.dataset.type;
         
+        console.log(target.dataset.type);
+        
         switch(target.dataset.type) {
             case "Individual": 
                 message.value = "Pozdrav, treba mi malo pomoći oko nekih oblasti, pa me zanimaju detalji za individualne časove.";
@@ -23,7 +25,7 @@ export function initContact() {
             case "ExamPreparation":
                 message.value = "Pozdrav, u frci sam sa odredjenim ispitom pa me zanima tačno kako funkcioniše paket spremanja ispita.";
                 break;    
-            case "ProjectAssignment":
+            case "Project":
                 message.value = "Pozdrav, imam seminarski rad koji moram hitno da rešim pa me zanimaju odredjeni detalji.";
                 break;
             default:
