b64105bf88d9aa1e357a91b0ad30a5f44732981a
Milan Nikolić <milan.miki.nikolic2002@gmail.com>
2026-06-24 22:33:11 +0200

    chore(migrations): Commit initial migrations

---
+++ CodeWithMixx/Migrations/20260624202904_FixATypeBugInIdentityRoleEntitity.cs
@@ -0,0 +1,139 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CodeWithMixx.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixATypeBugInIdentityRoleEntitity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AppRoleClaims_AspNetRoles_RoleId",
+                table: "AppRoleClaims");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_AppUserRoles_AspNetRoles_RoleId",
+                table: "AppUserRoles");
+
+            migrationBuilder.DropTable(
+                name: "AspNetRoles");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "NormalizedName",
+                table: "AppRoles",
+                type: "character varying(256)",
+                maxLength: 256,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "AppRoles",
+                type: "character varying(256)",
+                maxLength: 256,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "RoleNameIndex",
+                table: "AppRoles",
+                column: "NormalizedName",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AppRoleClaims_AppRoles_RoleId",
+                table: "AppRoleClaims",
+                column: "RoleId",
+                principalTable: "AppRoles",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AppUserRoles_AppRoles_RoleId",
+                table: "AppUserRoles",
+                column: "RoleId",
+                principalTable: "AppRoles",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AppRoleClaims_AppRoles_RoleId",
+                table: "AppRoleClaims");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_AppUserRoles_AppRoles_RoleId",
+                table: "AppUserRoles");
+
+            migrationBuilder.DropIndex(
+                name: "RoleNameIndex",
+                table: "AppRoles");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "NormalizedName",
+                table: "AppRoles",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(256)",
+                oldMaxLength: 256,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "AppRoles",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(256)",
+                oldMaxLength: 256,
+                oldNullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "AspNetRoles",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "text", nullable: false),
+                    ConcurrencyStamp = table.Column<string>(type: "text", nullable: true),
+                    Name = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: true),
+                    NormalizedName = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AspNetRoles", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "RoleNameIndex",
+                table: "AspNetRoles",
+                column: "NormalizedName",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AppRoleClaims_AspNetRoles_RoleId",
+                table: "AppRoleClaims",
+                column: "RoleId",
+                principalTable: "AspNetRoles",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AppUserRoles_AspNetRoles_RoleId",
+                table: "AppUserRoles",
+                column: "RoleId",
+                principalTable: "AspNetRoles",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
