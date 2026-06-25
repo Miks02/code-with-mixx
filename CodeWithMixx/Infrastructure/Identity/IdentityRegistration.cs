b3dc32cf1fd8797e7e60760f9d712122e6dad334
Milan Nikolić <milan.miki.nikolic2002@gmail.com>
2026-06-25 10:52:43 +0200

    feat(identity): Remove certain password requirements and add authZ role policies

--- CodeWithMixx/Infrastructure/Identity/IdentityRegistration.cs
+++ CodeWithMixx/Infrastructure/Identity/IdentityRegistration.cs
@@ -1,6 +1,7 @@
 using CodeWithMixx.Domain.AppUsers;
 using CodeWithMixx.Infrastructure.Persistence;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 namespace CodeWithMixx.Infrastructure.Identity;
 
@@ -14,10 +15,24 @@ public static class IdentityRegistration
             options.Password.RequireDigit = true;
             options.Password.RequiredLength = 8;
             options.Password.RequireLowercase = true;
+            options.Password.RequireUppercase = false;
+            options.Password.RequireNonAlphanumeric = false;
             options.SignIn.RequireConfirmedEmail = false;
             options.SignIn.RequireConfirmedAccount = false;
         })
         .AddEntityFrameworkStores<AppDbContext>()
         .AddDefaultTokenProviders();
+
+        services.AddAuthorizationBuilder()
+            .AddPolicy("AdminPolicy", policy =>
+            {
+                policy.RequireAuthenticatedUser();
+                policy.RequireRole("Admin");
+            })
+            .AddPolicy("StudentPolicy", policy =>
+            {
+                policy.RequireAuthenticatedUser();
+                policy.RequireRole("Student");
+            });
     }
 }
