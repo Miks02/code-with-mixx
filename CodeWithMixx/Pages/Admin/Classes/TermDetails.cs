6e34527c30fdcf08f33690d7a7331d4fff281c59
Milan Nikolić <milan.miki.nikolic2002@gmail.com>
2026-07-06 12:42:33 +0200

    wip(term-details): Add an empty termdetails partial that will be used to show a detailed view of a reserved term next to a list of classes

---
+++ CodeWithMixx/Pages/Admin/Classes/TermDetails.cs
@@ -0,0 +1,20 @@
+﻿namespace CodeWithMixx.Pages.Admin.Classes
+{
+    public record TermDetails
+    {
+        public int ReservationId { get; init; }
+        public string StudentName { get; init; } = string.Empty;
+        public string SubjectName { get; init; } = string.Empty;
+        public Decimal TotalPrice { get; init; }
+        public IReadOnlyList<ClassItem> Classes { get; init; } = [];
+        public int TotalClasses => Classes.Count;
+
+        public record ClassItem
+        {
+            public int ClassId { get; init; }
+            public string StartsAt { get; init; } = string.Empty;
+            public string EndsAt { get; init; } = string.Empty;
+            public Decimal Price { get; init; }
+        }
+    }
+}
