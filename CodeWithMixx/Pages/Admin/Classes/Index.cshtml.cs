fa68060d0d6696c5dd9b3c576e4188ccebe0390e
Milan Nikolić <milan.miki.nikolic2002@gmail.com>
2026-07-11 07:25:00 +0200

    feat(classes): Enhance IndexModel to support filtering and sorting for class retrieval

--- CodeWithMixx/Pages/Admin/Classes/Index.cshtml.cs
+++ CodeWithMixx/Pages/Admin/Classes/Index.cshtml.cs
@@ -1,19 +1,32 @@
+using CodeWithMixx.Pages.Admin.Classes.Create;
 using Htmx;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace CodeWithMixx.Pages.Admin.Classes;
 
-public class IndexModel(GetClassesPageHandler getPageHandler) : PageModel
+public class IndexModel(GetClassesPageHandler getPageHandler, GetClassesHandler getClassesHandler) : PageModel
 {
+    private readonly int _pageSize = 14;
+    
     public ClassPageViewModel ViewModel = new();
-    public async Task<IActionResult> OnGet(CancellationToken ct = default)
+    public async Task<IActionResult> OnGet([FromQuery] string? filter, [FromQuery] string? sort, [FromQuery] int? subjectId, [FromQuery] int page = 1, CancellationToken ct = default)
     {
-        ViewModel = await getPageHandler.HandleAsync("", "", 1, 10, ct);
+        ViewModel = await getPageHandler.HandleAsync(filter, sort, subjectId, page, _pageSize, ct);
 
         if(Request.IsHtmx())
             return Partial("_ClassPage", ViewModel);
 
         return Page();
+    }
+    
+    public async Task<IActionResult> OnGetClasses([FromQuery] string? filter, [FromQuery] string? sort, [FromQuery] int? subjectId, [FromQuery] int page = 1, CancellationToken ct = default)
+    {
+        var pagedClasses = await getClassesHandler.HandleAsync(filter, sort, subjectId, page, _pageSize, ct);
+
+        if(Request.IsHtmx())
+            return Partial("_ClassList", pagedClasses);
+
+        return RedirectToPage("/admin/classes/index", new {filter, sort, subjectId, page});
     }
 }
