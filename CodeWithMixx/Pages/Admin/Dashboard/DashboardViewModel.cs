84717208ef1c8e18166702ac3850b860c598f3f5
Milan Nikolić <milan.miki.nikolic2002@gmail.com>
2026-07-13 14:14:13 +0200

    feat(dashboard): Add yearly finance and student chart data to dashboard-handler and view-model

--- CodeWithMixx/Pages/Admin/Dashboard/DashboardViewModel.cs
+++ CodeWithMixx/Pages/Admin/Dashboard/DashboardViewModel.cs
@@ -6,6 +6,8 @@ public record DashboardViewModel
     public int CompletedClasses { get; init; }
     public int ActiveStudents { get; init; }
     public int ProjectsCount { get; init; }
+    public IReadOnlyList<int> Years { get; init; } = [];
+    public FinanceChart FinanceChartData { get; init; } = null!;
     public Dictionary<string, int> SubjectsChart { get; init; } = [];
     public IReadOnlyList<UpcomingTermItem> UpcomingTerms { get; init; } = []; 
 }
--- CodeWithMixx/Pages/Admin/Dashboard/GetDashboardHandler.cs
+++ CodeWithMixx/Pages/Admin/Dashboard/GetDashboardHandler.cs
@@ -10,7 +10,7 @@ namespace CodeWithMixx.Pages.Admin.Dashboard;
 
 public class GetDashboardHandler(AppDbContext context) : IHandler
 {
-    public async Task<DashboardViewModel> HandleAsync(CancellationToken ct)
+    public async Task<DashboardViewModel> HandleAsync(int? year, CancellationToken ct)
     {
         var totalIncome = await context.Reservations
             .Where(r => r.PaymentStatus == PaymentStatus.Paid || r.PaymentStatus == PaymentStatus.PartiallyPaid)
@@ -23,6 +23,11 @@ public class GetDashboardHandler(AppDbContext context) : IHandler
         var activeStudents = await context.Students
             .Where(s => s.AppUser.AccountStatus == AccountStatus.Active)
             .CountAsync(ct);
+
+        var years = await context.Classes
+            .Select(c => c.StartsAt.Year)
+            .Distinct()
+            .ToListAsync(ct);
         
         var upcomingTerms = await context.Reservations
             .Where(r => r.Classes.Any(c => c.StartsAt > DateTime.UtcNow))
@@ -41,31 +46,62 @@ public class GetDashboardHandler(AppDbContext context) : IHandler
             .Take(6)
             .ToListAsync(ct);
         
-        var classCountBySubject = await GetClassCountBySubject(ct);
-
-        foreach (var item in classCountBySubject)
-        {
-            var subject = item.Key;
-            var count = item.Value;
-            
-            Console.WriteLine($"Subject: {subject}, Count: {count}");
-            
-        }
-
         return new DashboardViewModel
         {
             TotalIncome = totalIncome,
             CompletedClasses = completedClasses,
             ActiveStudents = activeStudents,
-            SubjectsChart = classCountBySubject,
+            Years = years,
+            SubjectsChart = await GetClassCountBySubject(ct),
+            FinanceChartData = await GetStudentsAndIncomeByMonth(year, ct),
             UpcomingTerms = upcomingTerms
         };
 
+    }
+
+    public async Task<FinanceChart> GetStudentsAndIncomeByMonth(int? year, CancellationToken ct)
+    {
+        var selectedYear = year ?? DateTime.UtcNow.Year;
+        
+        var incomeData = await context.Reservations
+            .Where(r => r.Classes.Min(c => c.StartsAt).Year == selectedYear && (r.PaymentStatus == PaymentStatus.Paid || r.PaymentStatus == PaymentStatus.PartiallyPaid))
+            .GroupBy(r => r.Classes.Min(c => c.StartsAt).Month)
+            .Select(g => new {Month = g.Key, Income = g.Sum(r => r.PaidAmount)})
+            .ToDictionaryAsync(g => g.Month, g => g.Income, ct);
+        
+        
+        var studentData = await context.Classes
+            .Where(c => c.StartsAt.Year == selectedYear) 
+            .GroupBy(c => c.StartsAt.Month)             
+            .Select(g => new 
+            {
+                Month = g.Key,
+                StudentCount = g.Select(c => c.Reservation.StudentId).Distinct().Count()
+            })
+            .ToDictionaryAsync(x => x.Month, x => x.StudentCount, ct);
+        
+        var finalIncomeList = new List<decimal>();
+        var finalStudentList = new List<int>();
+
+        for(int i = 1; i <= 12; i++)
+        {
+            finalIncomeList.Add(incomeData.TryGetValue(i, out var value) ? value : 0);
+            finalStudentList.Add(studentData.TryGetValue(i, out var value1) ? value1 : 0);
+        }
+        
+        
+        return new FinanceChart
+        {
+            SelectedYear = selectedYear,
+            IncomeByMonth = finalIncomeList,
+            StudentsCountByMonth = finalStudentList
+        };
     }
     
     private async Task<Dictionary<string, int>> GetClassCountBySubject(CancellationToken ct)
     {
         return await context.Classes
+            .Where(c => c.StartsAt < DateTime.UtcNow && c.ClassStatus != ClassStatus.Cancelled)
             .GroupBy(c => c.Subject.Name)
             .Select(g => new {SubjectName = g.Key, Count = g.Count()})
             .ToDictionaryAsync(g => g.SubjectName, g => g.Count, ct);
