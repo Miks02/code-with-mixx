7296dbeac254382236354fecb3f18c45cb55efbf
Milan Nikolić <milan.miki.nikolic2002@gmail.com>
2026-07-30 09:11:26 +0200

    feat(projects-create): Add `CreateProjectInput` model and validator for project creation

---
+++ CodeWithMixx/Pages/Admin/Projects/Create/CreateProjectInput.cs
@@ -0,0 +1,47 @@
+using CodeWithMixx.Domain.Entities.Projects;
+using CodeWithMixx.Domain.Entities.Reservations;
+using FluentValidation;
+
+namespace CodeWithMixx.Pages.Admin.Projects.Create;
+
+public record CreateProjectInput
+{
+    public int SubjectId { get; init; }
+    public string StudentId { get; set; } = null!;
+    public string? Notes { get; init; }
+    public PaymentStatus PaymentStatus { get; init; } = PaymentStatus.Pending;
+    public ProjectStatus ProjectStatus { get; init; } = ProjectStatus.Standby;
+    public DateTime StartDate { get; init; } = DateTime.UtcNow;
+    public DateTime EndDate { get; init; } = DateTime.UtcNow.AddDays(7);
+    public decimal Price { get; init; }
+    public decimal PaidAmount { get; init; }
+    
+};
+
+public class CreateProjectValidator : AbstractValidator<CreateProjectInput>
+{
+    public CreateProjectValidator()
+    {
+        RuleFor(x => x.SubjectId)
+            .NotEmpty().WithMessage("Izaberite predmet projekta");
+        
+        RuleFor(x => x.StudentId)
+            .NotEmpty().WithMessage("Izaberite studenta.");
+        
+        RuleFor(x => x.Price)
+            .GreaterThanOrEqualTo(0).WithMessage("Cena mora biti pozitivan broj");
+
+        RuleFor(x => x.PaidAmount)
+            .GreaterThanOrEqualTo(0).WithMessage("Plaćeni iznos mora biti pozitivan broj");
+        
+        RuleFor(x => x.Notes)
+            .MaximumLength(500).WithMessage("Napomene ne mogu biti duže od 500 karaktera");
+        
+        RuleFor(x => x.StartDate)
+            .NotEmpty().WithMessage("Izaberite datum početka projekta");
+        
+        RuleFor(x => x.EndDate)
+            .NotEmpty().WithMessage("Izaberite datum završetka projekta")
+            .GreaterThan(x => x.StartDate).WithMessage("Kraj projekta mora biti nakon početka projekta");
+    }
+}
